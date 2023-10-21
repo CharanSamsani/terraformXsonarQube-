c5811cbe04a8af5af2c63710795cf612ea454519
Charan <89248547+Charan-Samsani@users.noreply.github.com>
2023-10-21 14:03:54 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "cidr" {
+  description = "This is the cidr"
+  default = "10.0.0.0/16"
+}
+
+variable "username" {
+  description = "The user for the ec2"
+  default = "ec2-user"
+}
+
+variable "instance_type" {
+  description = "The instance type you want to use"
+  default = "t2.medium"
+}
